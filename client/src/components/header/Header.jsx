1c0d6722c56b4cd28889db908afd11bc42ef4591
Jason Moritz <moritz.jason@gmail.com>
2021-11-10 17:30:51 -0600

    added breakpoint styling for nav and home pages.

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -19,56 +19,58 @@ export default function Header() {
 
   return (
     <>
-      <nav className='mobile-header'>
-        <Button
-          id='hamburger-button'
-          aria-controls='mobile-nav-menu'
-          aria-haspopup='true'
-          aria-expanded={open ? 'true' : undefined}
-          sx={{ p: 0, color: '#f9f8f8' }}
-          onClick={handleClick}
-        >
-          {toggle === null ? (
-            <MenuIcon sx={{ fontSize: 40, p: 0 }} />
-          ) : (
-            <CloseIcon sx={{ fontSize: 40, p: 0 }} />
-          )}
-        </Button>
-        <Menu
-          id='mobile-nav-menu'
-          anchorEl={toggle}
-          open={open}
-          onClose={handleClose}
-          transitionDuration={600}
-          PaperProps={{
-            style: {
-              minWidth: '350px',
-            },
-          }}
-        >
-          <MenuItem key='link-home'>
-            <NavHashLink className='mobile-nav-link' smooth to='#home'>
-              Home
-            </NavHashLink>
-          </MenuItem>
-          <MenuItem key='link-about'>
-            <NavHashLink className='mobile-nav-link' smooth to='#about'>
-              About
-            </NavHashLink>
-          </MenuItem>
-          <MenuItem key='link-project'>
-            <NavHashLink className='mobile-nav-link' smooth to='#project1'>
-              Projects
-            </NavHashLink>
-          </MenuItem>
-          <MenuItem key='link-contact'>
-            <NavHashLink className='mobile-nav-link' smooth to='#contact'>
-              Contact
-            </NavHashLink>
-          </MenuItem>
-        </Menu>
-        <div className='mobile-logo'>Jason Moritz</div>
-      </nav>
+      <HideOnScroll>
+        <nav className='mobile-header'>
+          <Button
+            id='hamburger-button'
+            aria-controls='mobile-nav-menu'
+            aria-haspopup='true'
+            aria-expanded={open ? 'true' : undefined}
+            sx={{ p: 0, color: '#f9f8f8' }}
+            onClick={handleClick}
+          >
+            {toggle === null ? (
+              <MenuIcon sx={{ fontSize: 40, p: 0 }} />
+            ) : (
+              <CloseIcon sx={{ fontSize: 40, p: 0 }} />
+            )}
+          </Button>
+          <Menu
+            id='mobile-nav-menu'
+            anchorEl={toggle}
+            open={open}
+            onClose={handleClose}
+            transitionDuration={600}
+            PaperProps={{
+              style: {
+                minWidth: '350px',
+              },
+            }}
+          >
+            <MenuItem key='link-home'>
+              <NavHashLink className='mobile-nav-link' smooth to='#home'>
+                Home
+              </NavHashLink>
+            </MenuItem>
+            <MenuItem key='link-about'>
+              <NavHashLink className='mobile-nav-link' smooth to='#about'>
+                About
+              </NavHashLink>
+            </MenuItem>
+            <MenuItem key='link-project'>
+              <NavHashLink className='mobile-nav-link' smooth to='#project1'>
+                Projects
+              </NavHashLink>
+            </MenuItem>
+            <MenuItem key='link-contact'>
+              <NavHashLink className='mobile-nav-link' smooth to='#contact'>
+                Contact
+              </NavHashLink>
+            </MenuItem>
+          </Menu>
+          <div className='mobile-logo'>Jason Moritz</div>
+        </nav>
+      </HideOnScroll>
 
       <HideOnScroll>
         <nav className='header'>
--- client/src/screens/home/Home.jsx
+++ client/src/screens/home/Home.jsx
@@ -4,24 +4,22 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 
 export default function Home() {
   return (
-    <>
-      <section id='home' className='home'>
-        <div className='welcome-text'>
-          Hello, I'm <span className='welcome-name'>Jason.</span>
-          <br />
-          <span className='welcome-text-span'>
-            I'm a full-stack web developer.
-          </span>
+    <section id='home' className='home'>
+      <div className='welcome-text'>
+        Hello, I'm <span className='welcome-name'>Jason.</span>
+        <br />
+        <div className='welcome-text-secondary'>
+          I'm a full-stack web developer.
         </div>
-        <div className='bottom-link'>
-          <HashLink smooth to='#about'>
-            About me
-            <div>
-              <KeyboardArrowDownIcon />
-            </div>
-          </HashLink>
-        </div>
-      </section>
-    </>
+      </div>
+      <div className='bottom-link'>
+        <HashLink smooth to='#about'>
+          About me
+          <div>
+            <KeyboardArrowDownIcon />
+          </div>
+        </HashLink>
+      </div>
+    </section>
   )
 }

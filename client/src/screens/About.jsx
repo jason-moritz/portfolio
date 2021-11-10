8a3150aa6989281e563aba79b60c669f2a8ae45f
Jason Moritz <moritz.jason@gmail.com>
2021-11-09 20:18:33 -0600

    base styling for about section complete

--- client/src/screens/About.jsx
+++ client/src/screens/About.jsx
@@ -5,11 +5,15 @@ import './About.css'
 export default function About() {
   return (
     <section id='about' className='about'>
-      <div className='about-content'>
-        From a young age I've loved learning languages. Having lived abroad and
-        learning Korean and Japanese, I've turned my passion for languages
-        towards programming. I am a self-driven problem-solver who takes extreme
-        ownership to accomplish tasks, with the focus always on team success.
+      <div className='about-content-container'>
+        <h1 className='title'>About Me</h1>
+        <div className='about-content'>
+          From a young age I've loved learning languages. Having lived abroad
+          and learning Korean and Japanese, I've turned my passion for languages
+          towards programming. I am a self-driven problem-solver who takes
+          extreme ownership to accomplish tasks, with the focus always on team
+          success.
+        </div>
       </div>
       <div className='about-bottom-link'>
         <HashLink smooth to='#projects'>

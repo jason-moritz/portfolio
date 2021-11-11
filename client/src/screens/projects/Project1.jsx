9bbdc28a8443f03786cd6d8aac8168d2c790c36f
Jason Moritz <moritz.jason@gmail.com>
2021-11-11 08:32:44 -0600

    Full styling complete for Project1

--- client/src/screens/projects/Project1.jsx
+++ client/src/screens/projects/Project1.jsx
@@ -4,22 +4,25 @@ import Hubbub from '../../assets/hubbub-thumbnail.png'
 import { HashLink } from 'react-router-hash-link'
 import './Project1.css'
 
-export default function Projects() {
+export default function Project1() {
   return (
     <section id='project1' className='project1'>
-      <h1 className='screen-title'>Projects</h1>
       <div className='project-container'>
-        <div className='project-image-and-links'>
-          <img className='project-img' src={Hubbub} alt='project-preview' />
-        </div>
-        <div className='project-info'>
+        <div className='project-half-a'>
           <div className='project-title'>Hubbub</div>
-          <h3 className='project-description'>
-            Reddit-style forum website where users can post their thoughts,
-            upload pictures, and comment on other users' posts. This was a solo
-            project that features full user authentication with CRUD privileges
-            utilizing Bcrypt and JSON web tokens.
-          </h3>
+          <div className='project-img-container'>
+            <img className='project-img' src={Hubbub} alt='project-preview' />
+          </div>
+        </div>
+        <div className='project-half-b'>
+          <div className='project-info'>
+            <h3 className='project-description'>
+              Reddit-style forum website where users can post their thoughts,
+              upload pictures, and comment on other users' posts. This was a
+              solo project that features full user authentication with CRUD
+              privileges utilizing Bcrypt and JSON web tokens.
+            </h3>
+          </div>
           <div className='project-links'>
             <a
               className='project-link'
@@ -38,33 +41,39 @@ export default function Projects() {
               Deployed site
             </a>
           </div>
-          <h1 className='project-tech-stack'>Made With: </h1>
-          <ul>
-            <li className='list-item'>
-              <span>Ruby on Rails</span>
-            </li>
-            <li className='list-item'>
-              <span>React JS</span>
-            </li>
-            <li className='list-item'>
-              <span>PostgreSQL</span>
-            </li>
-            <li className='list-item'>
-              <span>Bcrypt</span>
-            </li>
-            <li className='list-item'>
-              <span>JWT</span>
-            </li>
-            <li className='list-item'>
-              <span>Material UI</span>
-            </li>
-            <li className='list-item'>
-              <span>Heroku</span>
-            </li>
-            <li className='list-item'>
-              <span>Netlify</span>
-            </li>
-          </ul>
+          <div className='project-tech-stack'>
+            <div className='tech-stack-title'>Made With: </div>
+            <div className='tech-stack-container'>
+              <ul>
+                <li className='list-item'>
+                  <span>Ruby on Rails</span>
+                </li>
+                <li className='list-item'>
+                  <span>React JS</span>
+                </li>
+                <li className='list-item'>
+                  <span>PostgreSQL</span>
+                </li>
+                <li className='list-item'>
+                  <span>Bcrypt</span>
+                </li>
+              </ul>
+              <ul>
+                <li className='list-item'>
+                  <span>JWT</span>
+                </li>
+                <li className='list-item'>
+                  <span>Material UI</span>
+                </li>
+                <li className='list-item'>
+                  <span>Heroku</span>
+                </li>
+                <li className='list-item'>
+                  <span>Netlify</span>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
       <div className='bottom-link'>
